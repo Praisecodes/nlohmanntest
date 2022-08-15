ce6004128f4e6710eccde4f0f22ab8e7ea0af991
Praisecodes <cmancoder@gmail.com>
2022-08-15 19:27:11 +0100

    Initial Commit

---
+++ nlohmannTest.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <map>
+#include <iterator>
+#include <vector>
+#include <string>
+
+using json = nlohmann::json;
+
+int main()
+{
+    std::map<std::string, std::string> details{};
+    std::string username, password;
+    std::cout << "Enter Your Username Please: ";
+    std::getline(std::cin, username);
+    std::cout << "Enter Your Password " << username << ": ";
+    std::getline(std::cin, password);
+    return 0;
+}
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Nlohmann JSON Header-Only C++ Library (Test)
+### Utilizing the [nlohmann/json](https://github.com/nlohmann/json) header-only library built by [@nlohmann](https://github.com/nlohmann) for a JSON project in C++
+---
+
